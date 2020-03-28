970c3079732aa29b5ae00daefa82d4c71981cb3d
Luis Gonçalo <luisgoncalo@MacBook-Pro-de-Luis.local>
2020-03-28 00:21:55 +0000

    Classes updated

--- src/Components/Class/Class.js
+++ src/Components/Class/Class.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import './Class.css';
+import {Image} from "react-bootstrap";
+import car from './car.jpg'
 
 class Class extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { class: props.class };
+    }
+
     render() {
         return (
             <div>
-
+                <h3>{this.state.class.name}</h3>
+                <Image src={car} rounded />
+                <Image src={car} rounded />
             </div>
         );
     }
--- src/Components/Classes/Classes.js
+++ src/Components/Classes/Classes.js
@@ -4,19 +4,31 @@ import Title from "../Title/Title";
 import {getClasses} from "../../Utils";
 import Class from '../Class/Class'
 import {ListGroup} from "react-bootstrap";
+import {getColor} from "../../Colors";
 
 class Classes extends Component {
     render() {
         // Cria uma apresentação para cada classe
         const classes = getClasses();
+        let background_color;
+        let color;
+        let flag = true;
         const classes_comp = classes.map((c) => {
+            if(flag){
+                background_color = getColor();
+                color = 'white';
+                flag = false;
+            } else{
+                background_color = 'white';
+                color = 'black';
+                flag = true;
+            }
             return (
-                <ListGroup.Item> <Class className="col-lg" key={"Class " + c.id} class={c} /> </ListGroup.Item>
+                <ListGroup.Item style={{backgroundColor:background_color, color:color}}> <Class className="col-lg" key={"class_" + c.id} class={c} /> </ListGroup.Item>
             );
         });
 
         return (
-
             <div>
                 <Title name={"A nossa frota"}/>
                 <ListGroup>
