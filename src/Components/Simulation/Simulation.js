c756dc5521f192ad569ac45cbfada0152ca7739a
Luis Gonçalo <luisgoncalo@MacBook-Pro-de-Luis.local>
2020-03-28 18:10:49 +0000

    Final code implementation

--- src/Components/Simulation/Simulation.js
+++ src/Components/Simulation/Simulation.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import './Simulation.css';
+import "react-datepicker/dist/react-datepicker.css";
 import Title from "../Title/Title";
-import {Form, Button, Col} from "react-bootstrap";
+import {Form, Button, Col, Alert} from "react-bootstrap";
+import {createEmail} from "../../Utils";
+import DatePicker, { registerLocale } from "react-datepicker";
+import ptBR from "date-fns/locale/pt-BR";
+import { format } from 'date-fns'
 
 class Simulation extends Component {
     constructor(props) {
@@ -13,6 +18,9 @@ class Simulation extends Component {
             message: '',
             start_date: null,
             end_date: null,
+            show: false,
+            color: 'success',
+            alert: ''
         };
         this.handleNameChange = this.handleNameChange.bind(this);
         this.handleEmailChange = this.handleEmailChange.bind(this);
@@ -21,6 +29,10 @@ class Simulation extends Component {
         this.handleEndDateChange = this.handleEndDateChange.bind(this);
         this.handleMessageChange = this.handleMessageChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.createError = this.createError.bind(this);
+        this.createSuccess = this.createSuccess.bind(this);
+
+        registerLocale("ptBR", ptBR);
     }
 
     handleNameChange (evt) {
@@ -40,22 +52,43 @@ class Simulation extends Component {
     }
 
     handleStartDateChange (evt) {
-        this.setState({ start_date: evt.target.value });
+        // var formattedDate = format(evt, 'yyyy-MM-dd');
+        this.setState({ start_date: evt });
     }
 
     handleEndDateChange (evt) {
-        this.setState({ end_date: evt.target.value });
+        // var formattedDate = format(evt, 'yyyy-MM-dd');
+        this.setState({ end_date: evt });
+    }
+
+    createError(){
+        let new_color = 'alert';
+        let new_alert = 'O e-mail não foi enviado!';
+        this.setState({ show: !this.state.show, color: new_color, alert: new_alert });
+
+        this.timeoutId = setTimeout(function () {
+            this.setState({show: true});
+        }.bind(this), 1000);
+    }
+
+    createSuccess(){
+        let new_color = 'success';
+        let new_alert = 'O e-mail foi enviado com sucesso.';
+        this.setState({ show: !this.state.show, color: new_color, alert: new_alert });
+
+        this.timeoutId = setTimeout(function () {
+            this.setState({show: false});
+        }.bind(this), 5000);
     }
 
     handleSubmit(event) {
         event.preventDefault();
 
-        /*
-        console.log(this.state.name);
-        console.log(this.state.email);
-        console.log(this.state.car_type);
-        console.log(this.state.message);
-        */
+        const formatted_start_date = format(this.state.start_date, 'yyyy-MM-dd');
+        const formatted_end_date = format(this.state.end_date, 'yyyy-MM-dd');
+
+        const email_body = createEmail(this.state.email, this.state.name, this.state.car_type, formatted_start_date, formatted_end_date, this.state.message);
+        console.log(email_body);
 
         var elasticemail = require('elasticemail');
         var client = elasticemail.createClient({
@@ -65,18 +98,21 @@ class Simulation extends Component {
 
         var msg = {
             from: 'veriwa9198@itiomail.com',
-            from_name: 'Luis Agante',
-            to: 'lmagoncalo@gmail.com',
-            subject: 'Hello',
-            body_text: 'Hello World!'
+            from_name: this.state.name,
+            to: 'jessica.aparente@gmail.com',
+            subject: 'Pedido de simulação',
+            body_text: email_body
         };
 
+        var self = this;
         client.mailer.send(msg, function(err, result) {
             if (err) {
+                self.createError();
                 return console.error(err);
+            } else {
+                self.createSuccess();
+                return console.log(result);
             }
-
-            console.log(result);
         });
     }
 
@@ -100,44 +136,61 @@ class Simulation extends Component {
                     </Form.Group>
 
                     <h3>informação de contacto</h3>
-                    <Form.Group>
+                    <Form.Group onChange={this.handleCarTypeChange}>
                         <Col sm={10}>
                             <Form.Check
                                 type="radio"
-                                label="first radio"
+                                label="Pequenos Utilitários"
                                 name="formHorizontalRadios"
                                 id="formHorizontalRadios1"
+                                value="1"
                             />
                             <Form.Check
                                 type="radio"
-                                label="second radio"
+                                label="Utilitários"
                                 name="formHorizontalRadios"
                                 id="formHorizontalRadios2"
+                                value="2"
                             />
                             <Form.Check
                                 type="radio"
-                                label="third radio"
+                                label="Utilitários Económicos"
                                 name="formHorizontalRadios"
                                 id="formHorizontalRadios3"
+                                value="3"
                             />
                         </Col>
                         <Col sm={10}>
                             <Form.Check
                                 type="radio"
-                                label="fourth radio"
+                                label="Carrinhas"
                                 name="formHorizontalRadios"
                                 id="formHorizontalRadios4"
+                                value="4"
                             />
                             <Form.Check
                                 type="radio"
-                                label="fifth radio"
+                                label="Monovolumes"
                                 name="formHorizontalRadios"
                                 id="formHorizontalRadios5"
+                                value="5"
                             />
                         </Col>
                     </Form.Group>
 
                     <h3>data pretendida</h3>
+
+                    <DatePicker
+                        selected={this.state.start_date}
+                        dateFormat="dd/MM/yyyy"
+                        onChange={this.handleStartDateChange}
+                    />
+
+                    <DatePicker
+                        selected={this.state.end_date}
+                        dateFormat="dd/MM/yyyy"
+                        onChange={this.handleEndDateChange}
+                    />
 
                     <h3>mensagem</h3>
                     <Form.Group>
@@ -147,6 +200,9 @@ class Simulation extends Component {
                     <Button variant="outline-dark" type="submit">
                         submeter pedido
                     </Button>
+                    <Alert variant={this.state.color} show={this.state.show}>
+                        {this.state.alert}
+                    </Alert>
                 </Form>
             </div>
         );
--- src/Utils.js
+++ src/Utils.js
@@ -52,3 +52,36 @@ export function getClasses() {
         },
     ]
 }
+
+export function createEmail(email, name, car_type, start_date, end_date, message) {
+    let string = "";
+
+    string += "Nome: " + name + "\n";
+    string += "Email: " + email + "\n";
+    let car_name;
+    switch (car_type) {
+        case "1":
+            car_name = "Pequeno utilitário";
+            break;
+        case "2":
+            car_name = "Utilitário";
+            break;
+        case "3":
+            car_name = "Utilitário económico";
+            break;
+        case "4":
+            car_name = "Carrinha";
+            break;
+        case "5":
+            car_name = "Monovolume";
+            break;
+        default:
+            break
+    }
+    string += "Tipo de carro: " + car_name + "\n";
+    string += "Data: " + start_date + " até " + end_date + "\n";
+    string += "Mensagem: " + message + "\n";
+
+    return string;
+
+}
