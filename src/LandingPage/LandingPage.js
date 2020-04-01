7dda0b656b80eb9cc301baeccec1e6fbe11ef023
Luis Gonçalo <luisgoncalo@MacBook-Pro-de-Luis.local>
2020-04-01 12:05:53 +0100

    Links added

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from "react-router-dom";
 
 class LandingPage extends Component{
     render() {
@@ -6,6 +7,10 @@ class LandingPage extends Component{
         return (
             <div>
                 Landing Page
+                <br/>
+                <Link to="/rentacar">Rent-a-Car</Link>
+                <br/>
+                <Link to="/seguros">Seguros</Link>
             </div>
         );
     }
